ad31c741c0a6d1813a7d46150475e894a393adb0
Raul Martinez Jr <rmartinez@usautoparts.com>
2020-02-19 18:00:51 +0800

    Named placeholder parameters

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ fn main() {
     println!("Hello, world from main.rs");
     print::run();
 
+    //basic formatting
     println!("{} is from {}", "Brad", "Los Angeles");
+
+    //positional arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Brad", "Los Angeles", "code");
+
+    //named arguments
+    println!("{name} likes to play {activity}", name = "John", activity = "Soccer");
 }
