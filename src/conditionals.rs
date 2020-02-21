98866be6c6d2f09cabbf2f376209d2c65aeec08b
Raul Martinez Jr <rmartinez@usautoparts.com>
2020-02-21 20:17:36 +0800

    Conditionals if/elseif/else and shorthand

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,13 +1,21 @@
 pub fn run()
 {
-
-    let age = 18;
+    let age: u8 = 22;
+    let check_id: bool = true;
+    let knows_person_of_age = true;
 
     //if else
-    if age >= 21 {
+    if age >= 21 && check_id || knows_person_of_age {
         println!("Bartender says: What do you want to drink?");
-    } else {
+    } else if age <21 && check_id {
         println!("Bartender says: Sorry you have to leave..");
+    } else {
+        println!("Bartender says: I need to see your ID");       
     }
 
+    //no ternary operator in Rust
+    //here's a shorthand
+    let is_of_age = if age >= 21 { true } else { false };
+
+    println!("is_of_age {:?} ", is_of_age);
 }
