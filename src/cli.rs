367fb83c2c265d0b9b98d377081da64cd24e8615
Raul Martinez Jr <rmartinez@usautoparts.com>
2020-02-25 15:26:07 +0800

    cli

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,12 @@ use std::env;
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
+    let command = args[1].clone();  //why do you have to use .clone()??
 
     println!("\nargs: {:?}", args);
+    println!("\ncommand: {}", command);    
+
+    if command ==  "hello" {
+        println!("Hey there. Hello!");
+    }
 }
